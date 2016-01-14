e199db9041ced69b845432ee3e43c8ac1e867a70
alice <1303957245@qq.com>
2016-01-14 19:54:06 +0800

    add address

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
 Q1: hello,can you help me ?
 sure
 But,what is your question?
+https://github.com/xiaocong/uiautomator
+https://github.com/xiaocong/uiautomator.git
+http://zh-google-styleguide.readthedocs.org/en/latest/google-python-styleguide/python_language_rules/
