c8748ab76378badf4aeff9839acc3b69fa503f95
alice <1303957245@qq.com>
2016-01-15 10:32:54 +0800

    Q2,pls wufeng help me

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 Q1: hello,can you help me ?
 sure
 But,what is your question?
-https://github.com/xiaocong/uiautomator
-https://github.com/xiaocong/uiautomator.git
-http://zh-google-styleguide.readthedocs.org/en/latest/google-python-styleguide/python_language_rules/
+
+
+Q2：
+
+class Person(object):
+        def _init_(self,name):
+            self.name=name
+        def sayhello(self):
+            print 'My name is:',self.name
+if __name__ == '__main__':   
+    c=Person("bill")
+    print c
+	
+	
+	
+	 c=Person("bill")这句错误，但是
+	我要调用类里面的sayhello（）函数，怎么把name这个参数传进去。
+	
+	 c=Person()
+    print c.sayhello（"bill"）  这样子也不行
